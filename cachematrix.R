d747741f4f1aab94944103774a615d2977637d9c
alisonpope <alisonjpope@gmail.com>
2014-07-26 13:16:04 +0100

    Updated version for readability.
    Simplified inline comments and split code into logical segments for
    improved readability.

--- cachematrix.R
+++ cachematrix.R
@@ -1,48 +1,61 @@
 ## These two functions can be used to store (cache) a matrix (x) and the computed inverse
 ## of that matrix (m) in a special type of matrix that contains variables to hold
-## both matrices and a list of sub-functions taht can be used as methods
+## both matrices and a list of sub-functions that can be used as methods
 ## to get and set both the matrix and its inverse.
 
-## This function creates the special type of matrix.  
+## makeCacheMatrix(): This function creates the special type of matrix.  
 ## The initial matrix should be passed as an argument.  If missing an empty matrix will be created.
 ## The initial matrix is held in variable x.
 ## The cached inverse of the matrix if computed is held in m.  m will initially be null.
 ## There is also a list of sub-functions to set and get the values of x and m
-## Once an instance of the special matrix has been created cacheSolve should be used to
-## compute the inverse.
+## Once an instance of the special matrix has been created cacheSolve should be used to compute the inverse.
 
-makeCacheMatrix <- function(x = matrix()) { # initialise the special matrix providing a standard matrix as an argument
-  m <- NULL # sets inverse to null initially as hasn't been computed
-  set <- function(y) { # sub-function to set the value of x to a new matrix
-    x <<- y # will set the parent function x variable to the value of y passed to sub-function
-    m <<- NULL # will set parent function m to Null to clear cache as matrix has changed
-  }
-  get <- function() x # just returns the value of x i.e the standard matrix
-  setinverse <- function(inverse) m <<- inverse # sets the value of m to the inverse of x (used by cacheSolve function)
-  getinverse <- function() m # just returns the value of m i.e the cached inverse matrix
+makeCacheMatrix <- function(x = matrix()) { 
+  
+  m <- NULL 
+  
+  # sub-functions to set and get the stored standard matrix
+  
+  set <- function(y) { 
+      x <<- y 
+      m <<- NULL 
+    }
+  get <- function() x 
+  
+  # sub-functions to set and get the stored inverse matrix cache
+  
+  setinverse <- function(inverse) m <<- inverse 
+  getinverse <- function() m 
+  
+  # puts the sub-functions into a list so they can be easily accessed
+  
   list(set = set, get = get,
        setinverse = setinverse,
-       getinverse = getinverse) # puts the sub-functions into a list
+       getinverse = getinverse) 
 }
 
-## This function will return the inverse of a provided matrix x.
-## x cannot be a standard R matrix but must be a special matrix create using the 
-## makeCacheMatrix function.
-## If the inverse has already been computed and stored in cache (m) then this function
-## will return the cached value.
+## cacheSolve(): This function will return the inverse of a provided special matrix x.
+## x cannot be a standard R matrix but must be a special matrix create using the makeCacheMatrix() function.
+## If the inverse has already been computed and stored in cache (m) then this function will return the cached value.
 ## If m is null then this function will compute the inverse, cahce it in m and return it.
 
 cacheSolve <- function(x, ...) {
+  
   ## Return a matrix that is the inverse of 'x'
   
-  m <- x$getinverse() # get the value of the cached inverse variable, m, from the provided special matrix
-  if(!is.null(m)) { # if m has a value return the cached value for m
+  # If there is a cached inverse matrix stored in variable m of the special matrix then return the cached value
+  
+  m <- x$getinverse() 
+  if(!is.null(m)) { 
     message("getting cached data")
     return(m)
   }
   
-  data <- x$get() # if m is null get the standard matrix stored in x variable of the special matrix
-  m <- solve(data, ...) # compute the inverse
-  x$setinverse(m) # cache the computed inverse in the m variable of the special matrix
-  m # return the computed inverse
+  # If variable m of the special matrix is empty then compute the inverse, store it in the cache then return it
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+  
 }
